4f8d9d27f0eb8dc703e17e568935b0c7924c3762
Murka <54054900+Murka007@users.noreply.github.com>
2022-07-19 00:46:21 +0400

    Update and rename gameGridRemoval.js to gameGridRemoval.user.js

--- moomoo/gameGridRemoval.user.js
+++ moomoo/gameGridRemoval.user.js
@@ -3,7 +3,7 @@
 // @author Murka
 // @description Removes the game grid
 // @icon https://moomoo.io/img/favicon.png?v=1
-// @version 0.1
+// @version 0.2
 // @match *://moomoo.io/*
 // @match *://*.moomoo.io/*
 // @run-at document-start
@@ -29,12 +29,32 @@
         return value > min && value < max;
     }
 
+    function createHook(target, prop, callback) {
+        const symbol = Symbol(prop);
+        Object.defineProperty(target, prop, {
+            get() { return this[symbol]; },
+            set(value) { callback(this, symbol, value); },
+            configurable: true
+        })
+    }
+
+    createHook(window, "config", function(that, symbol, value) {
+        if (typeof value === "object" && value.hasOwnProperty("maxScreenHeight")) {
+            delete window.config;
+            Object.defineProperty(window, "config", {
+                value: value,
+                configurable: false,
+                writeable: false
+            })
+        }
+    })
+
     CanvasRenderingContext2D.prototype.moveTo = new Proxy(CanvasRenderingContext2D.prototype.moveTo, {
         apply(target, _this, args) {
 
             if (!GRID_ENABLED) {
                 const [ x, y ] = args;
-                const { maxScreenWidth, maxScreenHeight } = window.config || {};
+                const { maxScreenWidth, maxScreenHeight } = window.config || {};;
                 if (inRange(x, 0, maxScreenWidth) || inRange(y, 0, maxScreenHeight)) return null;
             }
 
